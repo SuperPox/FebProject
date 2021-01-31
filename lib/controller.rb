3c3b2f5cb01ba6f737f23a012b1dad59316706a2
SuperPox <calebkohl@gmail.com>
2021-01-30 18:42:45 -0700

    added input name function

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,6 +1,22 @@
 class Controller
     
+    def initialize
+        @user_name = ""
+    end
+    
     def welcome 
-        puts "_______TEST______"
+        puts "/// THE DUNGEON ///"
+        self.prompt_for_name
+    end
+
+    def prompt_for_name
+        puts "What is your name: "
+        input = gets.strip
+        @user_name = input.capitalize
+        self.verify_name
+    end
+
+    def verify_name
+        puts "Welcome to the Dungeon #{@user_name}"
     end
 end
